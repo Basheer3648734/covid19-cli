716090747f826f7b6e7cdfba7e6a48c3978eb76b
Basheer <bk3648734@gmail.com>
2020-06-08 20:36:09 +0530

    added welcome message

--- commands.js
+++ commands.js
@@ -1,9 +1,13 @@
 const program = require("commander");
+const chalk = require("chalk");
 const table = require("./table");
+
 program
   .command("list")
   .option("-s,--state <name>", "name of the state")
   .action((optionData) => {
+    console.log(chalk.bold.blue("welcome to covid19-cli"));
+    console.log(chalk.bold.blue("---------------------------"));
     if (!optionData.state) {
       table();
     } else {
